69afdce3b71896e329ee33b86c7eef736848caac
munaALB <boamuna@yahoo.com>
2019-03-07 23:22:45 +0200

    php pages and connect

---
+++ includes/footer.php
@@ -0,0 +1,71 @@
+  <footer class="footer-area p_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3  col-md-6 col-sm-6">
+                        <div class="single-footer-widget tp_widgets">
+                           <h6 class="footer_title">About the company</h6>
+							<p>Cooperation and establish different training courses
+								that have been delivered in Libya.</p>
+							<p>Conducted a full Study in urea Water to be applied in agricultural
+								projects.</p>
+							<p>Support Petrofact in their contract with NOC
+								for building a technichal training Centre in Bengahzi. </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget tp_widgets">
+						   <h6 class="footer_title">Our Address in Libya</h6>
+						   <p><i class="fa fa-thumb-tack"></i> Wali Alahed Street <br> Tripoli, Libya</p>
+						   <p><i class="fa fa-phone"></i> +218-913781866 </p>
+						   <p><i class="fa fa-envelop"></i></p>
+						   
+						   <h6 class="footer_title">Our Address in Egypt</h6>
+						   <p><i class="fa fa-thumb-tack"></i> 59 Mohi El-din AbuElezz ST. Dokki <br> Cairo, Egypt</p>
+						   <p><i class="fa fa-phone"></i> 0020 1026198899 / 0020 1553709190 </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-2  col-md-6 col-sm-6">
+                        <div class="single-footer-widget tp_widgets">
+                           <h6 class="footer_title">Quick Links</h6>
+                            <ul class="list">
+                            	<li><a href="#">Home Page</a></li>
+                            	<li><a href="#">About Us</a></li>
+                            	<li><a href="#">Our Courses</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <aside class="f_widget news_widget">
+        					<div class="f_title">
+        						<h3 class="footer_title">Newsletter</h3>
+        					</div>
+        					<p>Stay updated with our latest trends</p>
+        					<div id="mc_embed_signup">
+                                <form target="_blank" action="" method="get" class="subscribe_form relative">
+                                	<div class="input-group d-flex flex-row">
+                                        <input name="EMAIL" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                                        <button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>		
+                                    </div>				
+									<div class="mt-10 info"></div><br>
+									or contact us directly via email :
+									<a href="mailto:info@massader.org" class="mail">info@massader.org</a>
+                                </form>
+                            </div>
+        				</aside>
+                    </div>
+                </div>
+                <div class="row footer-bottom d-flex justify-content-between align-items-center">
+                    <p class="col-lg-8 col-md-8 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          			Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Designed by <a href="#" target="_blank">Rima Rhaim</a></p>
+                    <div class="col-lg-4 col-md-4 footer-social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-dribbble"></i></a>
+                        <a href="#"><i class="fa fa-behance"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+		
